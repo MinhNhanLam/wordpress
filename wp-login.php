ab3a5e2f008e0134b29ce745b917b468131bf95c
MinhNhanLam <leviusnhan681@gmail.com>
2022-10-30 20:18:29 +0700

    dddd

--- wp-login.php
+++ wp-login.php
@@ -3,7 +3,7 @@
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Đăng nhập &lsaquo; My Wordpress &#8212; WordPress</title>
-	<meta name="robots" content="noindex, nofollow, noarchive">
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
 <link rel="stylesheet" id="dashicons-css" href="https://minhnhanlam.github.io/wordpress/wp-includes/css/dashicons.min.css?ver=6.0.3" type="text/css" media="all">
 <link rel="stylesheet" id="buttons-css" href="https://minhnhanlam.github.io/wordpress/wp-includes/css/buttons.min.css?ver=6.0.3" type="text/css" media="all">
@@ -103,7 +103,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 <script type="text/javascript" src="https://minhnhanlam.github.io/wordpress/wp-includes/js/wp-util.min.js?ver=6.0.3" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"07db0cb97f"};
+var userProfileL10n = {"user_id":"0","nonce":"82dc79403d"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">( function( domain, translations ) {
