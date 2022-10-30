1aaa5796fd0b8b6d9bad7a3aa53aa5a8a1ffcca7
MinhNhanLam <leviusnhan681@gmail.com>
2022-10-30 17:23:14 +0700

    posttt

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://minhnhanlam.github.io/wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmyproject%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://minhnhanlam.github.io/wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmyproject%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://minhnhanlam.github.io/wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmyproject%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1">https://minhnhanlam.github.io/wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmyproject%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1</a></p>
+	</body>
+</html>
